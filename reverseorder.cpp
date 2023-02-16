73610efc8efd81048542c7e2dba673c4d2233596
ThomasH242 <Thomas.Huang45@myhunter.cuny.edu>
2023-02-16 15:37:47 -0500

    2/16

--- reverseorder.cpp
+++ reverseorder.cpp
@@ -12,19 +12,24 @@ void reverse_order(std::string date1, std::string date2){
     getline(fin, junk); 
     double eastSt, eastEl, westSt, westEl; 
     int c = 0;
-    int co =0;
-    double arr[999]; double newarr[999];
+    int co = 0;
+    double arr[999]; 
+    double newarr[999];
+    std::string datearr[999];
+    std::string ndatearr[999];
     std::string datetsv;
     while(fin >> datetsv >> eastSt >> eastEl >> westSt >> westEl) { 
         if(datetsv >= date1 && datetsv <= date2){
             arr[c] = westEl;
+            datearr[c] = datetsv;
             c++;
         }
         fin.ignore(INT_MAX, '\n');
     }
      for(int i = c-1; i >= 0; i--){//last one has to be 583.18
         newarr[i] = arr[co];
-        std::cout << arr[i] << std::endl;
+        ndatearr[i] = datearr[co];
+        std::cout << ndatearr[i]<< " " << arr[i] << std::endl;
         co++;
     }
     fin.close();
