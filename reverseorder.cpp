174cefa19427b441d0134f509098cecaae20c1cb
ThomasH242 <Thomas.Huang45@myhunter.cuny.edu>
2023-02-16 15:58:47 -0500

    2/16

--- reverseorder.cpp
+++ reverseorder.cpp
@@ -13,11 +13,14 @@ void reverse_order(std::string date1, std::string date2){
     double eastSt, eastEl, westSt, westEl; 
     int c = 0;
     int co = 0;
+    //West Basin Elevation
     double arr[999]; 
     double newarr[999];
+    //dates
     std::string datearr[999];
     std::string ndatearr[999];
     std::string datetsv;
+    //PREP ^^^ ---------------------------------------------------------
     while(fin >> datetsv >> eastSt >> eastEl >> westSt >> westEl) { 
         if(datetsv >= date1 && datetsv <= date2){
             arr[c] = westEl;
@@ -26,7 +29,7 @@ void reverse_order(std::string date1, std::string date2){
         }
         fin.ignore(INT_MAX, '\n');
     }
-     for(int i = c-1; i >= 0; i--){//last one has to be 583.18
+     for(int i = c-1; i >= 0; i--){
         newarr[i] = arr[co];
         ndatearr[i] = datearr[co];
         std::cout << ndatearr[i]<< " " << arr[i] << std::endl;
