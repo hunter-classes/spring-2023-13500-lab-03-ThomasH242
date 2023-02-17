4340fdc71649e2eb2ea84f6c690a76a606c5798b
ThomasH242 <Thomas.Huang45@myhunter.cuny.edu>
2023-02-17 09:33:10 -0500

    added comments

--- reverseorder.cpp
+++ reverseorder.cpp
@@ -11,28 +11,30 @@ void reverse_order(std::string date1, std::string date2){
     std::string junk;
     getline(fin, junk); 
     double eastSt, eastEl, westSt, westEl; 
+    // counts ----------------------------------------------------------
     int c = 0;
     int co = 0;
-    //West Basin Elevation
+    //West Basin Elevation ---------------------------------------------
     double arr[999]; 
     double newarr[999];
-    //dates
+    //dates-------------------------------------------------------------
     std::string datearr[999];
-    std::string ndatearr[999];
+    std::string newdatearr[999];
     std::string datetsv;
-    //PREP ^^^ ---------------------------------------------------------
+    //WORK ------------------------------------------------------------
     while(fin >> datetsv >> eastSt >> eastEl >> westSt >> westEl) { 
-        if(datetsv >= date1 && datetsv <= date2){
-            arr[c] = westEl;
-            datearr[c] = datetsv;
+        if(datetsv >= date1 && datetsv <= date2){// if date is within start date and end date
+            arr[c] = westEl;        //arr[index] = elevation
+            datearr[c] = datetsv;   //datesarray[index] = date
             c++;
         }
         fin.ignore(INT_MAX, '\n');
     }
-     for(int i = c-1; i >= 0; i--){
+    //This reverses the old arrays and put them in new arrays----------
+     for(int i = c-1; i >= 0; i--){//i equals count
         newarr[i] = arr[co];
-        ndatearr[i] = datearr[co];
-        std::cout << ndatearr[i]<< " " << arr[i] << std::endl;
+        newdatearr[i] = datearr[co];
+        std::cout << newdatearr[i]<< " " << arr[i] << std::endl;
         co++;
     }
     fin.close();
